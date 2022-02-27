deaf6eb50e40e85fc46a30b0e3d5f253fd555c45
Felipe Maeda <maedaxd@gmail.com>
2022-02-27 19:18:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -49,3 +49,22 @@ class Blockchain():
         new_block = Block(transactions, previous_block_hash)
         self.chain.append(new_block)
 
+    def validate_chain(self):
+        for i in range(1,len(self.chain)):
+            current = self.chain[i]
+            previous = self.chain[i-1]
+            if current.hash != current.generate_hash():
+                print(f'The current hash of the block does not equal the generated hash of the block.')
+                return False
+            if previous.hash != previous.generate_hash():
+                print(f'The previous block\'s hash does not equal the previous hash value stored in the current block.')
+                return False
+        return True
+
+        def proof_of_work(self, block, difficulty=2):
+            proof = block.generate_hash()
+            while proof[:difficulty] != '0'*difficulty:
+                block.nonce += 1
+                proof = block.generate_hash()
+            block.nonce = 0
+            return proof
